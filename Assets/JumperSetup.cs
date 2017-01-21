f1234123db175e0e75313e79b437da968374eda8
DallonF <dallon.feldner@gmail.com>
2017-01-21 17:09:15 -0600

    Randomize player positions

---
+++ Assets/JumperSetup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class JumperSetup : MonoBehaviour
+{
+    public KeyCode[] PlayerKeys;
+
+    void Start()
+    {
+        var jumpers = FindObjectsOfType<InitJumper>().ToList();
+        foreach (var key in PlayerKeys)
+        {
+            // Assign a key to a random jumper
+            var rand = jumpers[Random.Range(0, jumpers.Count)];
+            jumpers.Remove(rand);
+            rand.InitPlayer(key);
+        }
+        // Everybody else is NPCs
+        foreach (var jumper in jumpers)
+        {
+            jumper.InitNPC();
+        }
+    }
+}
