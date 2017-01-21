ac8ce6016e28225abe8a98b377659d3549b1a6e8
DallonF <dallon.feldner@gmail.com>
2017-01-21 11:44:38 -0600

    Got a countdown timer

---
+++ Assets/Scripts/GameTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameTime : MonoBehaviour
+{
+    public float TimeRemaining = 2 * 60;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        TimeRemaining -= Time.deltaTime;
+        if (TimeRemaining <= 0)
+        {
+            // TODO: game over?
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Timer : MonoBehaviour
+{
+    private Text text;
+    private GameTime gameTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+        gameTime = FindObjectOfType<GameTime>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float timeRemaining = gameTime.TimeRemaining;
+        int minutesRemaining = Mathf.FloorToInt(timeRemaining / 60);
+        int secondsRemaining = Mathf.FloorToInt(timeRemaining % 60);
+
+        var textTimer = System.String.Format("{0}:{1}", minutesRemaining, secondsRemaining.ToString().PadLeft(2, '0'));
+        text.text = textTimer;
+    }
+}
