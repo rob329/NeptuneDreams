55700971f7255be4f85c93844889e0500d09246c
DallonF <dallon.feldner@gmail.com>
2017-01-21 16:39:02 -0600

    Init playerse

---
+++ Assets/InitJumper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitJumper : MonoBehaviour
+{
+    public GameObject KeyLabelPrefab;
+    public Transform LabelSpot;
+
+    void Start()
+    {
+        InitPlayer(KeyCode.Q);
+    }
+
+    public void InitPlayer(KeyCode key)
+    {
+        var controller = gameObject.AddComponent<PlayerJumperControl>();
+        controller.Key = key;
+        var keyLabel = GameObject.Instantiate(KeyLabelPrefab, LabelSpot, false);
+        keyLabel.transform.localPosition = Vector3.zero;
+        keyLabel.GetComponentInChildren<Text>().text = key.ToString();
+    }
+    
+    public void InitNPC()
+    {
+        
+    }
+}
