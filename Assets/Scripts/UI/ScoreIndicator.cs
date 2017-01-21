33b760281953697d8bd78d7d2ce3ee56212a588e
DallonF <dallon.feldner@gmail.com>
2017-01-21 12:04:52 -0600

    Add a score indicator

---
+++ Assets/Scripts/UI/ScoreIndicator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class ScoreIndicator : MonoBehaviour
+{
+    private Text text;
+    private GameScore gameScore;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        gameScore = FindObjectOfType<GameScore>();
+        UpdateText();
+    }
+
+    void FixedUpdate()
+    {
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        // Format number with commas
+        text.text = String.Format("Score: {0:N0}", gameScore.Score);
+    }
+}
