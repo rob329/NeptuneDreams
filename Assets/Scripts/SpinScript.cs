ef3a10887ee332e703a2c34f935051196fc6ab96
JennRaye <jtbont@live.com>
2017-01-21 23:10:56 -0600

    C4N YU 3SCAP3 SP1NNY CAT

---
+++ Assets/Scripts/SpinScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinScript : MonoBehaviour {
+	public Transform trans;
+	public float Speed;
+	// Use this for initialization
+	void Start () {
+		trans = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		trans.eulerAngles = new Vector3 (trans.eulerAngles.x, trans.eulerAngles.y, trans.eulerAngles.z + Speed * Time.deltaTime);
+	}
+}
