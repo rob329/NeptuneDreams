a9c288a5ab6aa185bc8d83875869e1cac0f8d82f
DallonF <dallon.feldner@gmail.com>
2017-01-22 16:22:45 -0600

    Fixed aspect ratio on Macs

---
+++ Assets/Scripts/CameraFix.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFix : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        float expectedAspectRatio = 16f / 9f;
+        var camera = GetComponent<Camera>();
+        float expectedWidth = camera.orthographicSize * expectedAspectRatio;
+        float newHeight = (1 / camera.aspect) * expectedWidth;
+        camera.orthographicSize = newHeight;
+    }
+}
