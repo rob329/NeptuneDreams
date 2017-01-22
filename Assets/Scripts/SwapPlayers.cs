1d8d7e7eaac17348d1da213f0d9235be1cc324ee
JennRaye <jtbont@live.com>
2017-01-21 18:54:45 -0600

    Added SwapPlayers script and object
    also some height fixes for jumpers

--- Assets/Scripts/SwapPlayers.cs
+++ Assets/Scripts/SwapPlayers.cs
@@ -3,14 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwapPlayers : MonoBehaviour {
+	public Transform[] Players;
+	public Transform[] CPUs;
+	public int PlayerToSwap;
+	public int CPUToSwap;
 
+	public Transform Swap1;
+	public Transform Swap2;
+
+	public float Distance;
+
+	public Transform CenterSwap;
+	public float TimerToSwap;
+
+	public float Speed;
+	public bool Swapping;
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (TimerToSwap > 8) {
+			PlayerToSwap = Random.Range (0, Players.Length);
+			CPUToSwap = Random.Range (0, CPUs.Length);
+
+			Swap1 = Players [PlayerToSwap];
+			Swap2 = CPUs [CPUToSwap];
+
+
+			if (Swap1.position.x < Swap2.position.x) {
+				Distance = Swap2.position.x - Swap1.position.x;
+				CenterSwap.position = new Vector3 (Swap1.position.x + (Distance/2), CenterSwap.position.y, CenterSwap.position.z);
+			} else {
+				Distance = Swap1.position.x - Swap2.position.x;
+				CenterSwap.position = new Vector3 (Swap2.position.x + (Distance/2), CenterSwap.position.y, CenterSwap.position.z);
+			}
+
+			Swapping = true;
+			Swap1.parent = CenterSwap;
+			Swap2.parent = CenterSwap;
+			Swap1.position = new Vector3 (Swap1.position.x, -2.77f, Swap1.position.z);
+			Swap2.position = new Vector3 (Swap2.position.x, -2.77f, Swap2.position.z);
+
+			Swap1.gameObject.GetComponent<Jumper> ().enabled = false;
+			Swap2.gameObject.GetComponent<Jumper> ().enabled = false;
+
+
+			TimerToSwap = 0;
+		} else {
+			TimerToSwap += Time.deltaTime;
+		}
+
+		if (Swapping == true) {
+			Swap1.eulerAngles = new Vector3 (Swap1.eulerAngles.x, Swap1.eulerAngles.y, 0);
+			Swap2.eulerAngles = new Vector3 (Swap2.eulerAngles.x, Swap2.eulerAngles.y, 0);
+
+			CenterSwap.eulerAngles = new Vector3 (CenterSwap.eulerAngles.x, CenterSwap.eulerAngles.y, CenterSwap.eulerAngles.z + (Time.deltaTime * Speed));
+
+			if (CenterSwap.eulerAngles.z >= 180) {
+				CenterSwap.eulerAngles = new Vector3 (CenterSwap.eulerAngles.x, CenterSwap.eulerAngles.y, 180);
+				Swap1.eulerAngles = new Vector3 (Swap1.eulerAngles.x, Swap1.eulerAngles.y, 0);
+				Swap2.eulerAngles = new Vector3 (Swap2.eulerAngles.x, Swap2.eulerAngles.y, 0);
+				Swap1.parent = null;
+				Swap2.parent = null;
+
+				CenterSwap.eulerAngles = new Vector3 (CenterSwap.eulerAngles.x, CenterSwap.eulerAngles.y, 0);
+				Swap1.position = new Vector3 (Swap1.position.x, -2.77f, Swap1.position.z);
+				Swap2.position = new Vector3 (Swap2.position.x, -2.77f, Swap2.position.z);
+				Swap1.gameObject.GetComponent<Jumper> ().enabled = true;
+				Swap2.gameObject.GetComponent<Jumper> ().enabled = true;
+				Swapping = false;
+			}
+		}
 	}
 }
